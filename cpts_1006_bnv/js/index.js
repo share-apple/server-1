6355147a9f80a01af829342fd914a380c17aeace
share-apple <1447576443@qq.com>
2018-08-31 14:55:12 +0800

    two

---
+++ cpts_1006_bnv/js/index.js
@@ -0,0 +1,9 @@
+const express =require('express');
+const path =require('path');
+const server =express();
+server.get('/',(req,res)=>{
+    res.sendFile(
+        path.resolve('./index.html')
+    )
+});
+server.listen(80);
